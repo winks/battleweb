33a6c99c1eef05d609fddc215fb4c5fb46dc04aa
Florian Anderiasch <fa@art-core.org>
2011-10-01 01:36:42 +0200

    Add guilds, items, css

--- src/battleweb/views/common.clj
+++ src/battleweb/views/common.clj
@@ -1,4 +1,6 @@
 (ns battleweb.views.common
+  (:require [battlenet.defs :as bnd]
+            [battlenet.tools :as bnt])
   (:use noir.core
         hiccup.core
         hiccup.page-helpers))
@@ -7,7 +9,8 @@
             (html5
               [:head
                [:title "battleweb"]
-               (include-css "/css/reset.css")]
+               (include-css "/css/reset.css")
+               (include-css "/css/battleweb.css")]
               [:body
                [:div#wrapper
                 content]]))
@@ -34,11 +37,45 @@
   [{:keys [name status slug type queue population battlegroup]}]
   [:li {:id slug, :class (if (= true status) "online" "offline")}
    [:h3 (str name " (" battlegroup ") [" type "]")]
-   [:span.pop (str "Pop: " population " (" (if (= false queue) "No ") "Queue)")]
-  ])
+   [:span.pop (str "Pop: " population " (" (if (= false queue) "No ") "Queue)")]])
 
 
 (defpartial realms-list
   [realms]
   [:ul#realmItems
    (map realm-item realms)])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defpartial guild-info
+  [region {:keys [name realm level side achievementPoints emblem lastModified]}]
+  [:div#guild
+   [:h3 {:class (.toLowerCase (nth bnd/bn-factions side))}
+    (str name " (" (.toUpperCase region) "-" realm ")")]
+   [:span.level level]
+   [:br]
+   [:span.points (str achievementPoints)]])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defpartial item-info
+  [region {:keys [name id description requiredLevel quality
+                  itemBind itemClass baseArmor itemLevel inventoryType
+                  sellPrice bonusStats icon]}]
+  [:div#item
+   [:h3 {:class (.toLowerCase (nth bnd/bn-quality quality))}
+    (str name)]
+   [:div {:class "icon"} [:img {:src (bnt/media-url-icon region "wow" "large" icon)}]]
+   [:span (if (= 1 itemBind) "Binds when picked up" "Binds when equipped")] [:br]
+   [:span (if (integer? inventoryType) (nth bnd/bn-inventory (+ -1 inventoryType)) "")] [:br]
+   [:span (if (integer? baseArmor) (str baseArmor " Armor"))] [:br]
+   [:span (str "Requires Level " requiredLevel)] [:br]
+   [:span (str "Item Level " itemLevel)] [:br]
+   [:span.item-description description]
+   [:span (str "Sell Price: "
+               (quot sellPrice 10000) " G " 
+               (str (quot (- sellPrice (* 10000 (quot sellPrice 10000))) 100) " S ")
+               (mod sellPrice 1000) " C")] [:br]
+   [:span.item-footer
+    [:a {:href (str "http://" region ".battle.net/wow/en/item/" id)} "Armory"] " "
+    [:a {:href (str "http://www.wowhead.com/item=" id)} "Wowhead"]]])
--- src/battleweb/views/welcome.clj
+++ src/battleweb/views/welcome.clj
@@ -1,7 +1,8 @@
 (ns battleweb.views.welcome
   (:require [battleweb.views.common :as common]
             [noir.content.pages :as pages]
-            [battlenet.core :as bnc])
+            [battlenet.core :as bnc]
+            [battlenet.network :as bnn])
   (:use noir.core
         hiccup.core
         hiccup.page-helpers))
@@ -22,3 +23,15 @@
     (common/layout
       [:h1 "Realms!"]
       (common/realms-list realms))))
+
+(defpage "/guild/:region/:realm/:name" {:keys [region realm name]}
+  (let [guild (bnn/read-remote-guild region realm name)]
+    (common/layout
+      [:h1 "Guilds!"]
+      (common/guild-info region guild))))
+
+(defpage "/item/:id" {:keys [id]}
+  (let [item (bnn/read-remote-item "eu" id)]
+    (common/layout
+      [:h1 "Items!"]
+      (common/item-info "eu" item))))
