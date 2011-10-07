02751d0940e2c31609349d162d0241216d9b3473
Florian Anderiasch <fa@art-core.org>
2011-10-07 15:14:25 +0200

    Added configurable port

--- src/battleweb/server.clj
+++ src/battleweb/server.clj
@@ -1,11 +1,12 @@
 (ns battleweb.server
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server])
+  (:use battleweb.settings))
 
 (server/load-views "src/battleweb/views/")
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))]
+        port (Integer. (get (System/getenv) "PORT" bw-port))]
     (server/start port {:mode mode
                         :ns 'battleweb})))
 
